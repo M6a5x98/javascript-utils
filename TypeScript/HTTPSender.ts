c9638a5381d0ca02f9b02db056270e60f00dc3d6
M6a5x98 <m6a5x98@outlook.com>
2024-12-19 20:53:21 +0100

    Added post request in HTTPSender.ts and fixed issues

--- TypeScript/HTTPSender.ts
+++ TypeScript/HTTPSender.ts
@@ -1,46 +1,72 @@
 type RequestOptions = {
   headers?: Record<string, string>;
-  type?: type;
+  type?: XMLHttpRequestResponseType;
 };
-type type = "json" | "text" | "html";
+type PostRequestOptions = {
+  headers?: Record<string, string>;
+  type?: XMLHttpRequestResponseType;
+  body?: XMLBodyType;
+};
+
+type XMLResponseType = object | string | ArrayBuffer | Blob | Document | null;
+type XMLBodyType = Document | XMLHttpRequestBodyInit | null;
 
 class HTTPSender {
   constructor() {}
-  public async GET(url: string, options: RequestOptions): Promise<any> {
+  public async get(
+    url: string,
+    Options: RequestOptions
+  ): Promise<XMLResponseType> {
     return new Promise((resolve, reject) => {
       if (typeof url !== "string")
         reject("String expected for url arg, received " + typeof url);
-      const type = options.type ?? "text";
+      const options = Options ?? {};
+      const type = (options.type ?? "text") as XMLHttpRequestResponseType;
       const headers = options.headers ?? {};
       const xhr = new XMLHttpRequest();
       xhr.open("GET", url);
       Object.keys(headers).forEach((header) => {
         xhr.setRequestHeader(header, headers[header]);
       });
-      xhr.withCredentials = false;
+      xhr.responseType = type;
       xhr.onload = () => {
-        switch (type) {
-          case "html":
-            resolve(xhr.responseXML);
-            break;
-          case "json":
-            try {
-              let response = JSON.parse(xhr.responseText);
-              resolve(response);
-            } catch (e) {
-              reject(e);
-            }
-            break;
-          default:
-            resolve(xhr.responseText);
-            break;
-        }
+        resolve({ body: xhr.response, status: xhr.status });
       };
       xhr.onerror = () => {
-        reject(xhr.status);
+        reject({ body: undefined, status: xhr.status });
       };
       try {
         xhr.send();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+  public async post(
+    url: string,
+    Options: PostRequestOptions
+  ): Promise<XMLResponseType> {
+    return new Promise((resolve, reject) => {
+      if (typeof url !== "string")
+        reject("String expected for url arg, received " + typeof url);
+      const options = Options ?? {};
+      const type = (options.type ?? "text") as XMLHttpRequestResponseType;
+      const headers = options.headers ?? {};
+      const body = options.body ?? null;
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", url);
+      Object.keys(headers).forEach((header) => {
+        xhr.setRequestHeader(header, headers[header]);
+      });
+      xhr.responseType = type;
+      xhr.onload = () => {
+        resolve({ body: xhr.response, status: xhr.status });
+      };
+      xhr.onerror = () => {
+        reject({ body: undefined, status: xhr.status });
+      };
+      try {
+        xhr.send(body);
       } catch (error) {
         reject(error);
       }
